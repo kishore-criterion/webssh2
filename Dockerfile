1d7905749e75ee84fd8b26720721a58c13676640
kishore1919 <kishore@criterionnetworks.com>
2023-09-22 13:24:22 +0530

    Added changes for cn

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM node:16
 
 WORKDIR /usr/src
-COPY app/ /usr/src/
-RUN npm install --production
-EXPOSE 21002
-CMD npm run start
 
+COPY app/ /usr/src/
+
+RUN npm ci --audit=false --bin-links=false --fund=false
+
+EXPOSE 21002/tcp
+
+RUN chmod +x ./docker-entrypoint.sh
+
+ENTRYPOINT [ "bash","./docker-entrypoint.sh" ]
--- app/index.js
+++ app/index.js
@@ -19,14 +19,17 @@ server.listen({ host: config.listen.ip, port: config.listen.port });
 console.log(`WebSSH2 service listening on ${config.listen.ip}:${config.listen.port}`);
 
 server.on('error', (err) => {
-  if (err.code === 'EADDRINUSE') {
-    config.listen.port += 1;
-    console.warn(`WebSSH2 Address in use, retrying on port ${config.listen.port}`);
-    setTimeout(() => {
-      server.listen(config.listen.port);
-    }, 250);
-  } else {
-    // eslint-disable-next-line no-console
-    console.log(`WebSSH2 server.listen ERROR: ${err.code}`);
-  }
+  // if (err.code === 'EADDRINUSE') {
+  //   config.listen.port += 1;
+  //   console.warn(`WebSSH2 Address in use, retrying on port ${config.listen.port}`);
+  //   setTimeout(() => {
+  //     server.listen(config.listen.port);
+  //   }, 250);
+  // } else {
+  //   // eslint-disable-next-line no-console
+  //   console.log(`WebSSH2 server.listen ERROR: ${err.code}`);
+  // }
+  // console.warn(`WebSSH2 server.listen ERROR: ${err.code}`);
+  console.log(`WebSSH2 server.listen ERROR: ${err.code}`);
+
 });
